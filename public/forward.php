8540e87117b9f33510cc660e6c25ef10f49ea741
lsardon <70115421+lsardon@users.noreply.github.com>
2025-09-03 19:43:59 -0500

    Create forward.php

---
+++ public/forward.php
@@ -0,0 +1,37 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use Twilio\TwiML\VoiceResponse;
+
+$response = new VoiceResponse();
+
+// Get POST data
+$from = $_POST['From'] ?? '';
+$addOns = json_decode($_POST['AddOns'] ?? '{}', true);
+
+// Count spam indicators
+$spamCount = 0;
+
+// Check each add-on if present
+if (isset($addOns['results']['nomorobo_spamscore']['result']['score'])) {
+    if ($addOns['results']['nomorobo_spamscore']['result']['score'] > 0.5) {
+        $spamCount++;
+    }
+}
+
+if (isset($addOns['results']['marchex_cleancall']['result']['result']['recommendation'])) {
+    if ($addOns['results']['marchex_cleancall']['result']['result']['recommendation'] === 'BLOCK') {
+        $spamCount++;
+    }
+}
+
+// Simple decision
+if ($spamCount >= 2) {
+    $response->say('This call has been blocked due to spam detection.');
+    $response->hangup();
+} else {
+    // Forward to your number or Growably
+    $response->dial('+12145500953'); // Your number
+}
+
+header('Content-Type: text/xml');
+echo $response;
